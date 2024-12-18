aa99823e25845fe1463be012b56ad975e5400234
Pierre ETRILLARD <pierre.etrillard@live.fr>
2024-12-18 11:03:09 +0100

    creation du routing et des principaux composants et services

---
+++ src/app/flights/control-pannel/control-pannel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-pannel',
+  standalone: true,
+  imports: [],
+  templateUrl: './control-pannel.component.html',
+  styleUrl: './control-pannel.component.scss'
+})
+export class ControlPannelComponent {
+
+}
