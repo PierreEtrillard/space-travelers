b84755aafee6defea842915a012076b9ca0353f7
Pierre ETRILLARD <pierre.etrillard@live.fr>
2025-01-09 18:04:19 +0100

    creation du panneau de selction des vols à te'rminer...

--- src/app/flights/flights.component.ts
+++ src/app/flights/flights.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
-import { SelectorPannelComponent } from "./selector-pannel/selector-pannel/selector-pannel.component";
-import { FlightsGridComponent } from "./flights-grid/flights-grid.component";
+import { Component, effect, inject } from '@angular/core';
+import { SelectorPannelComponent } from './selector-pannel/selector-pannel/selector-pannel.component';
+import { FlightsGridComponent } from './flights-grid/flights-grid.component';
 import { HeaderUtilService } from '../services/header-util.service';
+import { Store } from '@ngrx/store';
+import { FlightsActions } from '../store/actions/flights.actions';
+import { selectionFeature } from '../store/reducers/selection.reducer';
+import { flightsFeature } from '../store/reducers/flights.reducer';
 
 @Component({
   selector: 'app-flights',
   standalone: true,
   imports: [SelectorPannelComponent, FlightsGridComponent],
   templateUrl: './flights.component.html',
-  styleUrl: './flights.component.scss'
+  styleUrl: './flights.component.scss',
 })
 export class FlightsComponent {
-constructor(private headerService:HeaderUtilService){
-  
-}
+  store = inject(Store);
+  registredFlights = this.store.selectSignal(
+    flightsFeature.selectRegistredFlights
+  );
+  constructor(private headerService: HeaderUtilService) {
+    this.store.dispatch(FlightsActions.countFlights());
+    effect(() => {
+      if (this.registredFlights()) {        
+        this.headerService.subtitle.set(
+          `Laissez nous vous surprendre avec nos ${this.registredFlights()} vols actuellement programmés !`
+        );
+      }
+    },{allowSignalWrites:true});
+  }
 }
--- src/app/services/flights.service.ts
+++ src/app/services/flights.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Destination } from '../models/destination';
-import { map, Observable } from 'rxjs';
+import { from, map, Observable, switchMap } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Store } from '@ngrx/store';
 import { selectionFeature } from '../store/reducers/selection.reducer';
@@ -14,18 +14,38 @@ import { FlightsGroup } from '../models/flights-group';
 export class FlightsService {
   private httpOptions: any = { withCredentials: true };
   private apiUrl = environment.apiUrl;
-  private http=inject( HttpClient);
-  private store = inject(Store); 
-  private selection = this.store.selectSignal(selectionFeature.selectSelectionDetails)
+  private http = inject(HttpClient);
+  private store = inject(Store);
+  private selection = this.store.selectSignal(
+    selectionFeature.selectSelectionDetails
+  );
   destinations$ = this.http.get<Destination[]>(`${this.apiUrl}/destinations`);
-  destinations= toSignal(
+  destinations = toSignal(
     this.http.get<Destination[]>(`${this.apiUrl}/destinations`).pipe(
       map((res) => {
         return res;
       })
-    )
-  ,{initialValue:[]});
-  
-  getflights$= this.http.post<FlightsGroup[]>(`${this.apiUrl}/flights/search`,this.selection());
+    ),
+    { initialValue: [] }
+  );
 
+  getflights$ = this.http.post<FlightsGroup[]>(
+    `${this.apiUrl}/flights/search`,
+    this.selection()
+  );
+  countFlightsRegistred$ = this.http.get<number>(
+    `${this.apiUrl}/flights/count`
+  );
+  getTakeOffDatesFrom(station: string):Observable<Date[]> {
+    interface TakeOffDatesDto {
+      departure: Date;
+    }
+    return this.http
+      .post<TakeOffDatesDto[]>(`${this.apiUrl}/flights/from`, station)
+      .pipe(
+        map((takeOffList) =>
+          takeOffList.map((takeOffDate) => new Date(takeOffDate.departure))
+        )
+      );
+  }
 }
